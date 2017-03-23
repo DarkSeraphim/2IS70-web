0d06657c0008c5d37067820d3d8445c1edf348d4
TSlooff <t.slooff@hotmail.com>
2017-03-23 18:04:47 +0100

    minor change again

--- QuizMaster/settings.py
+++ QuizMaster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ue=7shpa!*mg7uo!!b^_24y=eive@r%rwj)m+0vy9)h3ainy$8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['2is70.pythonanywhere.com']
+ALLOWED_HOSTS = ['2is70.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
